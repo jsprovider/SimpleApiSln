f35ac16e82096101aa9618429571b7da510708ee
Jagath Sugathadasa <jsugathadasa@gmail.com>
2021-07-04 00:24:01 +1000

    Change the api output

--- SimpleApiSln.Api/Controllers/WeatherForecastController.cs
+++ SimpleApiSln.Api/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace SimpleApiSln.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing1", "Bracing1", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
